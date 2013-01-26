3c761b4c533e52e4b6bbaf2ec43baaa0efd40ede
adebeo <info@adebeo.com>
2013-01-26 17:38:36 +0100

    Signed-off-by: adebeo <info@adebeo.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-    # Returns the full title on a per-page basis.
+    # Returns the full title on a per-page basis.    
+    
+    def logo
+        image_tag("logo.png", :alt => "Application exemple", :class => "round")
+    end
+    
+      
     def titre
       base_title = "Ruby on Rails Tutorial Sample App"
       if @titre.empty?
